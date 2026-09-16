e4773c01a80494555ab44c724ba27f1f1d9bd6a9
mdhasanofc <mdhasanofc010@gmail.com>
2026-09-16 22:53:24 +0600

    Refresh cache after removing video download UI

--- live-cam-landing/sw.js
+++ live-cam-landing/sw.js
@@ -1,4 +1,4 @@
-const CACHE='livelens-v7-video-controls';
+const CACHE='livelens-v8-no-download';
 const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
